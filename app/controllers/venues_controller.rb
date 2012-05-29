fa0e4ede1aae95cf161f462f9a766371c13c80b7
Mark Hoad <markdhoad@gmail.com>
2012-05-29 22:41:19 +1000

    Create basic Venue controller actions

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,37 @@
 class VenuesController < ApplicationController
+
+  def index
+    @venues = Venue.paginate(:page => params[:page]).per_page(2)
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+    #render :layout => "show_venue"
+  end
+
+  def new
+    @venue = Venue.new
+    #@venue.trading_times.new 
+  end
+
+  def create
+    @venue = Venue.new(params[:venue])
+    if @venue.save
+      flash[:success] = "Venue sucessfully added"
+      redirect_to venue_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    if @venue.update_attributes(params[:venue])
+      flash[:success] = "Venue updated"
+      redirect_to @venue
+    else
+      render 'edit'
+    end
+  end
+
 end
--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe VenuesController do
 
+  before (:each) { @venue = FactoryGirl.create(:venue) }
+
+  describe "GET 'show'" do
+    
+    it "should be successful" do
+      get :show, :id => @venue.id
+      response.should be_success
+    end
+
+    it "should find the right venue" do
+      get :show, :id => @venue.id
+      assigns(:venue).should == @venue
+    end
+  end
+
 end
